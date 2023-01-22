6b1888194940825299f932b059737a2309766e42
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-01-22 15:48:34 +0100

    Fix internal release tag string

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 // Version
 define( 'FICTIONEER_VERSION', '5.0.0' );
 define( 'FICTIONEER_MAJOR_VERSION', '5' );
-define( 'FICTIONEER_RELEASE_TAG', '5.0.0' );
+define( 'FICTIONEER_RELEASE_TAG', 'v5.0.0' );
 
 if ( ! defined( 'CHILD_VERSION' ) ) {
   define( 'CHILD_VERSION', 0 );
