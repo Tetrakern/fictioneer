622d42da45b485a7af01c2e5c5d5655af607a984
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-02-08 19:51:48 +0100

    Set up endpoint

--- includes/functions/_api.php
+++ includes/functions/_api.php
@@ -1,5 +1,39 @@
 <?php
 
+// =============================================================================
+// FICTION API - GET ALL STORIES
+// =============================================================================
 
+if ( TRUE ) {
+  add_action(
+    'rest_api_init',
+    function () {
+      register_rest_route(
+        'storygraph/v1',
+        '/stories',
+        array(
+          'methods' => 'GET',
+          'callback' => 'fictioneer_api_get_stories',
+          'permission_callback' => '__return_true'
+        )
+      );
+    }
+  );
+}
+
+if ( ! function_exists( 'fictioneer_api_get_stories' ) ) {
+  /**
+   * Returns JSON with all stories plus meta data
+   *
+   * @since Fictioneer 5.1
+   * @link https://developer.wordpress.org/rest-api/extending-the-rest-api/adding-custom-endpoints/
+   *
+   * @param WP_REST_Request $WP_REST_Request Request object.
+   */
+
+  function fictioneer_api_get_stories( WP_REST_Request $data ) {
+    return rest_ensure_response( ['foo' => 'bar'] );
+  }
+}
 
 ?>
