32b5a904ea6be0f1d8b96f8c6b240de323c48de0
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-03-10 16:54:19 +0100

    Add missing parameters to docu

--- includes/functions/_shortcodes.php
+++ includes/functions/_shortcodes.php
@@ -386,18 +386,20 @@ add_shortcode( 'fictioneer_chapter_cards', 'fictioneer_shortcode_latest_chapters
  *
  * @since 3.0
  *
- * @param string|null $attr['count']      Optional. Maximum number of items. Default 4.
- * @param string|null $attr['author']     Optional. Limit items to a specific author.
- * @param string|null $attr['type']       Optional. Choose between 'default' and 'compact'.
- * @param string|null $attr['orderby']    Optional. Order argument. Default 'date'.
- * @param string|null $attr['post_ids']   Optional. Limit items to specific post IDs.
- * @param string|null $attr['categories'] Optional. Limit items to specific category names.
- * @param string|null $attr['tags']       Optional. Limit items to specific tag names.
- * @param string|null $attr['fandoms']    Optional. Limit items to specific fandom names.
- * @param string|null $attr['genres']     Optional. Limit items to specific genre names.
- * @param string|null $attr['characters'] Optional. Limit items to specific character names.
- * @param string|null $attr['rel']        Optional. Relationship between taxonomies. Default 'AND'.
- * @param string|null $attr['class']      Optional. Additional CSS classes, separated by whitespace.
+ * @param string|null $attr['count']           Optional. Maximum number of items. Default 4.
+ * @param string|null $attr['author']          Optional. Limit items to a specific author.
+ * @param string|null $attr['type']            Optional. Choose between 'default' and 'compact'.
+ * @param string|null $attr['orderby']         Optional. Order argument. Default 'date'.
+ * @param string|null $attr['post_ids']        Optional. Limit items to specific post IDs.
+ * @param string|null $attr['exclude_tag_ids'] Optional. Exclude posts with these tags.
+ * @param string|null $attr['exclude_cat_ids'] Optional. Exclude posts with these categories.
+ * @param string|null $attr['categories']      Optional. Limit items to specific category names.
+ * @param string|null $attr['tags']            Optional. Limit items to specific tag names.
+ * @param string|null $attr['fandoms']         Optional. Limit items to specific fandom names.
+ * @param string|null $attr['genres']          Optional. Limit items to specific genre names.
+ * @param string|null $attr['characters']      Optional. Limit items to specific character names.
+ * @param string|null $attr['rel']             Optional. Relationship between taxonomies. Default 'AND'.
+ * @param string|null $attr['class']           Optional. Additional CSS classes, separated by whitespace.
  *
  * @return string The rendered shortcode HTML.
  */
