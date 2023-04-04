b7939c9c96760e6cfed649b0e22e33a694814c5c
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-04-05 01:44:37 +0200

    Bump version to 5.2.3

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@
 // =============================================================================
 
 // Version
-define( 'FICTIONEER_VERSION', '5.2.2' );
+define( 'FICTIONEER_VERSION', '5.2.3' );
 define( 'FICTIONEER_MAJOR_VERSION', '5' );
-define( 'FICTIONEER_RELEASE_TAG', 'v5.2.2' );
+define( 'FICTIONEER_RELEASE_TAG', 'v5.2.3' );
 
 if ( ! defined( 'CHILD_VERSION' ) ) {
   define( 'CHILD_VERSION', false );
