2f44626e258f318730b6566bd4b57fa577168550
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-08-11 11:51:26 +0200

    Apply fcn_edit_others_files cap

--- includes/functions/_roles.php
+++ includes/functions/_roles.php
@@ -591,14 +591,10 @@ if ( ! current_user_can( 'manage_options' ) ) {
       return $caps;
     }
 
-    // if ( 'edit_post' != $cap && 'delete_post' != $cap ) {
-    //   return $caps;
-    // }
-
     // Get the post in question.
-    $post = get_post( $args[0] );
+    $post = get_post( $args[0] ?? 0 );
 
-    // Check if an attachment and whether the user is the owner (author)
+    // Check if an attachment and whether the user is the author (uploader)
     if (
       empty( $post ) ||
       $post->post_type != 'attachment' ||
@@ -615,6 +611,47 @@ if ( ! current_user_can( 'manage_options' ) ) {
     add_filter( 'map_meta_cap', 'fictioneer_edit_others_files', 9999, 4 );
   }
 
+  // === DELETE_READ_OTHERS_FILES ==============================================
+
+  /**
+   * User cannot delete the files of others
+   *
+   * @since Fictioneer 5.6.0
+   *
+   * @param array  $caps     Primitive capabilities required of the user.
+   * @param string $cap      Capability being checked.
+   * @param int    $user_id  The user ID.
+   * @param array  $args     Adds context to the capability check, typically
+   *                         starting with an object ID.
+   *
+   * @return array The still allowed primitive capabilities of the user.
+   */
+
+  function fictioneer_delete_others_files( $caps, $cap, $user_id, $args ) {
+    // Skip unrelated capabilities
+    if ( $cap != 'delete_post' ) {
+      return $caps;
+    }
+
+    // Get the post in question.
+    $post = get_post( $args[0] ?? 0 );
+
+    // Check if an attachment and whether the user is the author (uploader)
+    if (
+      empty( $post ) ||
+      $post->post_type != 'attachment' ||
+      $post->post_author == $user_id
+    ) {
+      return $caps;
+    }
+
+    // Disallow
+    return ['do_not_allow'];
+  }
+
+  if ( ! current_user_can( 'fcn_edit_others_files' ) ) {
+    add_filter( 'map_meta_cap', 'fictioneer_delete_others_files', 9999, 4 );
+  }
 
 
 }
