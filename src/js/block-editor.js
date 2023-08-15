e8894b5db7415b96cbc7db4181db6f6e36789462
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-08-15 17:33:18 +0200

    Fix editor script

--- src/js/block-editor.js
+++ src/js/block-editor.js
@@ -10,13 +10,11 @@ wp.domReady(function() {
       if (element.textContent === 'Stick to the top of the blog') {
         if (!fictioneerData.userCapabilities?.fcn_make_sticky) {
           element.closest('.components-panel__row').remove();
-          return;
         }
       }
 
       if (element.textContent === 'Allow pingbacks & trackbacks') {
         element.closest('.components-panel__row').remove();
-        return;
       }
     });
   }, 200); // Wait for async; not great, not terrible solution
