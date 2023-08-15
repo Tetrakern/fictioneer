ca5fe764dc2042db7b93522e8455f19a84d1c75a
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-08-15 13:31:03 +0200

    Remove ping-/trackback checkbox

--- src/js/block-editor.js
+++ src/js/block-editor.js
@@ -1,18 +1,26 @@
 
 // =============================================================================
-// RESTRICT STICKY CHECKBOX
+// RESTRICT EDITOR ELEMENTS (ENGLISH ONLY)
 // =============================================================================
 
 wp.domReady(function() {
 
   setTimeout(() => {
+
     document.querySelectorAll('label').forEach(element => {
       if (element.textContent === 'Stick to the top of the blog') {
         if (!fictioneerData.userCapabilities?.fcn_make_sticky) {
           element.closest('.components-panel__row').remove();
+          return;
         }
       }
+
+      if (element.textContent === 'Allow pingbacks & trackbacks') {
+        element.closest('.components-panel__row').remove();
+        return;
+      }
     });
-  }, 100);
+
+  }, 200); // Wait for async; not great, not terrible solution
 
 });
