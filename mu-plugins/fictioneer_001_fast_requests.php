c2f4e4e8e8b9db6d9f668e55bdeff9fe602e434c
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-08-24 02:37:15 +0200

    Exclude cache plugins from MU plugin

--- mu-plugins/fictioneer_001_fast_requests.php
+++ mu-plugins/fictioneer_001_fast_requests.php
@@ -22,16 +22,6 @@ if (
 
 $request_uri = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
 
-// Check if AJAX comment request
-if (
-  get_option( 'fictioneer_enable_fast_ajax_comments' ) &&
-  ( defined( 'DOING_AJAX' ) && DOING_AJAX ) &&
-  strpos( $_REQUEST['action'] ?? '', 'fictioneer_ajax' ) === 0 &&
-  strpos( $_REQUEST['action'] ?? '', '_comment' ) !== false
-) {
-  add_filter( 'option_active_plugins', 'fictioneer_exclude_plugins' );
-}
-
 // Check REST Request
 if ( strpos( $request_uri, 'wp-json/fictioneer/' ) !== false ) {
   add_filter( 'option_active_plugins', 'fictioneer_exclude_plugins' );
@@ -51,6 +41,53 @@ function fictioneer_exclude_plugins( $plugins ) {
   // Setup
   $allow_list = array(
     'fictioneer-email-subscriptions/fictioneer-email-subscriptions.php'
+  );
+
+  // Remove not allowed plugins
+  foreach ( $plugins as $index => $plugin ) {
+    if ( ! in_array( $plugin, $allow_list ) ) {
+      unset( $plugins[ $index ] );
+    }
+  }
+
+  // Continue filter
+  return $plugins;
+}
+
+// Check if AJAX comment request
+if (
+  get_option( 'fictioneer_enable_fast_ajax_comments' ) &&
+  ( defined( 'DOING_AJAX' ) && DOING_AJAX ) &&
+  strpos( $_REQUEST['action'] ?? '', 'fictioneer_ajax' ) === 0 &&
+  strpos( $_REQUEST['action'] ?? '', '_comment' ) !== false
+) {
+  add_filter( 'option_active_plugins', 'fictioneer_exclude_plugins_while_commenting' );
+}
+
+/**
+ * Filters the list of active plugins when commenting
+ *
+ * @since 1.0.0
+ *
+ * @param array $plugins  An array of active plugin paths.
+ *
+ * @return array Filtered array of active plugins.
+ */
+
+function fictioneer_exclude_plugins_while_commenting( $plugins ) {
+  // Setup
+  $allow_list = array(
+    'w3-total-cache/w3-total-cache.php', // W3 Total Cache
+    'wp-super-cache/wp-cache.php', // WP Super Cache
+    'wp-rocket/wp-rocket.php', // WP Rocket
+    'litespeed-cache/litespeed-cache.php', // LiteSpeed Cache
+    'wp-fastest-cache/wpFastestCache.php', // WP Fastest Cache
+    'cache-enabler/cache-enabler.php', // Cache Enabler
+    'hummingbird-performance/wp-hummingbird.php', // Hummingbird – Optimize Speed, Enable Cache
+    'wp-optimize/wp-optimize.php', // WP-Optimize - Clean, Compress, Cache
+    'sg-cachepress/sg-cachepress.php', // SG Optimizer (SiteGround)
+    'breeze/breeze.php', // Breeze (by Cloudways)
+    'nitropack/nitropack.php' // NitroPack
   );
 
   // Remove not allowed plugins
