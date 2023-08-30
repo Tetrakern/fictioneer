69463a2e092508898fed8f5a3f88fc1c9ba3141f
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-08-30 13:31:20 +0200

    Improve story CSS selectors

--- partials/_story-footer.php
+++ partials/_story-footer.php
@@ -36,27 +36,27 @@ $story = $args['story_data'];
 ?>
 
 <footer class="story__footer padding-left padding-right">
-  <div class="story__meta truncate _1-1">
-    <span class="story__status">
+  <div class="story__meta">
+    <span class="story__meta-item story__status">
       <i class="<?php echo $story['icon']; ?>"></i>
       <?php echo $story['status']; ?>
     </span>
-    <span class="story__date _published" title="<?php esc_attr_e( 'Published', 'fictioneer' ) ?>">
+    <span class="story__meta-item story__date _published" title="<?php esc_attr_e( 'Published', 'fictioneer' ) ?>">
       <i class="fa-solid fa-clock"></i>
       <span class="hide-below-480"><?php the_time( get_option( 'date_format' ) ); ?></span>
       <span class="show-below-480"><?php the_time( FICTIONEER_STORY_FOOTER_B480_DATE ); ?></span>
     </span>
-    <span class="story__words" title="<?php esc_attr_e( 'Total Words', 'fictioneer' ) ?>">
+    <span class="story__meta-item story__words" title="<?php esc_attr_e( 'Total Words', 'fictioneer' ) ?>">
       <i class="fa-solid fa-font"></i>
       <?php echo $story['word_count_short']; ?>
     </span>
-    <span class="story__rating" title="<?php esc_attr_e( 'Rating', 'fictioneer' ) ?>">
+    <span class="story__meta-item story__rating" title="<?php esc_attr_e( 'Rating', 'fictioneer' ) ?>">
       <i class="fa-solid fa-exclamation-circle"></i>
       <?php echo $story['rating']; ?>
     </span>
     <?php if ( $story['chapter_count'] > 0 ): ?>
       <button
-      class="checkmark story__meta-checkmark"
+      class="story__meta-item checkmark story__meta-checkmark"
       data-type="story"
       data-story-id="<?php echo $story_id; ?>"
       data-id="<?php echo $story_id; ?>"
