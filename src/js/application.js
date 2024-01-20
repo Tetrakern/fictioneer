75e4e0aa4ef1a1c58876485b1ca2dd6adae39613
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-01-20 15:04:43 +0100

    Move age confirmation script to application
    Make sure it is loaded.

--- src/js/application.js
+++ src/js/application.js
@@ -2017,3 +2017,52 @@ function fcn_markCurrentMenuItem() {
 
 // Initialize
 fcn_markCurrentMenuItem();
+
+// =============================================================================
+// AGE CONFIRMATION
+// =============================================================================
+
+if (_$$$('age-confirmation-modal') && localStorage.getItem('fcnAgeConfirmation') !== '1') {
+  // Delay to avoid impacting web vitals
+  setTimeout(() => {
+    fcn_showAgeConfirmationModal();
+  }, 2000);
+} else {
+  _$$$('age-confirmation-modal')?.remove();
+}
+
+/**
+ * Show age confirmation modal if required.
+ *
+ * @since 5.9.0
+ */
+
+function fcn_showAgeConfirmationModal() {
+  // Adult story or chapter?
+  const rating = _$('.story__article, .chapter__article')?.dataset.ageRating;
+
+  if (rating && rating !== 'adult') {
+    _$$$('age-confirmation-modal')?.remove();
+    return;
+  }
+
+  // Setup
+  const leave = _$$$('age-confirmation-leave');
+
+  // Disable site and show modal
+  fcn_theRoot.classList.add('age-modal-open');
+  _$$$('age-confirmation-modal').classList.add('_open');
+
+  // Confirm button
+  _$$$('age-confirmation-confirm')?.addEventListener('click', event => {
+    fcn_theRoot.classList.remove('age-modal-open');
+    event.currentTarget.closest('.modal').remove();
+    localStorage.setItem('fcnAgeConfirmation', '1');
+  });
+
+  // Leave button
+  leave?.addEventListener('click', () => {
+    window.location.href = leave.dataset.redirect ?? 'https://search.brave.com/';
+    localStorage.removeItem('fcnAgeConfirmation');
+  });
+}
