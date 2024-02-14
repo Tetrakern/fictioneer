192087c05122cc73cec275e2b17f98c1332b20c7
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-02-15 00:59:45 +0100

    Bump version to 5.11.0

--- functions.php
+++ functions.php
@@ -32,9 +32,9 @@ if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 // =============================================================================
 
 // Version
-define( 'FICTIONEER_VERSION', '5.10.0' );
+define( 'FICTIONEER_VERSION', '5.11.0' );
 define( 'FICTIONEER_MAJOR_VERSION', '5' );
-define( 'FICTIONEER_RELEASE_TAG', 'v5.10.0' );
+define( 'FICTIONEER_RELEASE_TAG', 'v5.11.0' );
 
 if ( ! defined( 'CHILD_VERSION' ) ) {
   define( 'CHILD_VERSION', null );
