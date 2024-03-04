d47af831769c579b5251ba43e66f0b7d70ca0ffd
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-03-05 00:03:25 +0100

    Bump version to 5.12.1

--- functions.php
+++ functions.php
@@ -32,9 +32,9 @@ if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 // =============================================================================
 
 // Version
-define( 'FICTIONEER_VERSION', '5.12.0' );
+define( 'FICTIONEER_VERSION', '5.12.1' );
 define( 'FICTIONEER_MAJOR_VERSION', '5' );
-define( 'FICTIONEER_RELEASE_TAG', 'v5.12.0' );
+define( 'FICTIONEER_RELEASE_TAG', 'v5.12.1' );
 
 if ( ! defined( 'CHILD_VERSION' ) ) {
   define( 'CHILD_VERSION', null );
--- readme.txt
+++ readme.txt
@@ -7,7 +7,7 @@ Contributors: tetrakern
 Requires at least: 6.1.0
 Tested up to: 6.4.2
 Requires PHP: 7.4
-Stable tag: 5.12.0
+Stable tag: 5.12.1
 License: GNU General Public License v3.0 or later
 License URI: http://www.gnu.org/licenses/gpl.html
 
