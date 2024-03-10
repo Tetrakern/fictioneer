c1c2ba6932319616bf76bc08f656cc581174089f
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-03-11 00:41:51 +0100

    Improve dialog modal action assignment

--- src/js/application.js
+++ src/js/application.js
@@ -1495,14 +1495,14 @@ fcn_theBody.querySelectorAll('.modal-toggle').forEach(element => {
 });
 
 // Open dialog modal
-_$$('[data-click-action="open-dialog-modal"]').forEach(element => {
+_$$('[data-click-action*="open-dialog-modal"]').forEach(element => {
   element.addEventListener('click', event => {
     document.querySelector(event.currentTarget.dataset.clickTarget).showModal();
   });
 });
 
 // Close dialog modal
-_$$('[data-click-action="close-dialog-modal"]').forEach(element => {
+_$$('[data-click-action*="close-dialog-modal"]').forEach(element => {
   element.addEventListener('click', event => {
     event.preventDefault();
     event.target.closest('dialog').close();
