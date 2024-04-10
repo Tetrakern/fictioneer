fe4e674e31052e402c26f8804c1f6f787f948416
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-04-10 12:10:55 +0200

    Add modifier classes to formatting modal rows
    Makes it easier to target them with CSS.

--- partials/_modal-formatting.php
+++ partials/_modal-formatting.php
@@ -23,61 +23,61 @@ defined( 'ABSPATH' ) OR exit;
     </label>
     <div class="modal__header drag-anchor"><?php echo fcntr( 'formatting_modal' ); ?></div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _darken">
       <i class="fa-solid fa-adjust reset setting-darken-reset" title="<?php esc_attr_e( 'Darken Background', 'fictioneer' ); ?>"></i>
       <input type="range" value="0" min="-1" max="1" step=".01" class="setting-darken-range" id="reader-settings-darken-range">
       <input type="number" value="0" min="-100" max="100" class="setting-darken-text" id="reader-settings-darken-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _saturation">
       <i class="fa-solid fa-droplet reset setting-saturation-reset" title="<?php esc_attr_e( 'Saturate Background', 'fictioneer' ); ?>"></i>
       <input type="range" value="0" min="-1" max="1" step=".01" class="setting-saturation-range" id="reader-settings-saturation-range">
       <input type="number" value="0" min="-100" max="100" class="setting-saturation-text" id="reader-settings-saturation-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing hide-below-640">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing hide-below-640 _width">
       <i class="fa-solid fa-arrows-alt-h reset" title="<?php esc_attr_e( 'Content Width', 'fictioneer' ); ?>" id="reader-settings-site-width-reset"></i>
       <input type="range" value="960" min="640" max="1920" step="1" id="reader-settings-site-width-range">
       <input type="number" value="960" min="640" max="1920" step="1" id="reader-settings-site-width-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _font-size">
       <i class="fa-solid fa-text-height reset" title="<?php esc_attr_e( 'Font Size (Percentage)', 'fictioneer' ); ?>" id="reader-settings-font-size-reset"></i>
       <input type="range" value="100" min="50" max="200" step="1" id="reader-settings-font-size-range">
       <input type="number" value="100" min="50" max="200" step="1" id="reader-settings-font-size-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _letter-spacing">
       <i class="fa-solid fa-text-width reset" title="<?php esc_attr_e( 'Letter Spacing', 'fictioneer' ); ?>" id="reader-settings-letter-spacing-reset"></i>
       <input type="range" value="0.0" min="-0.1" max="0.2" step=".001" id="reader-settings-letter-spacing-range">
       <input type="number" value="0.0" min="-0.1" max="0.2" step=".001" id="reader-settings-letter-spacing-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _line-height">
       <?php fictioneer_icon( 'line-height', 'reset', 'reader-settings-line-height-reset' ); ?>
       <input type="range" value="1.7" min="0.8" max="3.0" step=".1" id="reader-settings-line-height-range">
       <input type="number" value="1.7" min="0.8" max="3.0" step=".1" id="reader-settings-line-height-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _paragraph-spacing">
       <i class="fa-solid fa-paragraph reset" title="<?php esc_attr_e( 'Paragraph Spacing', 'fictioneer' ); ?>" id="reader-settings-paragraph-spacing-reset"></i>
       <input type="range" value="1.5" min="0" max="3.0" step=".05" id="reader-settings-paragraph-spacing-range">
       <input type="number" value="1.5" min="0" max="3.0" step=".05" id="reader-settings-paragraph-spacing-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _font-saturation">
       <i class="fa-solid fa-pen-nib reset" title="<?php esc_attr_e( 'Font Saturation', 'fictioneer' ); ?>" id="reader-settings-font-saturation-reset"></i>
       <input type="range" value="0" min="-1" max="1" step=".01" id="reader-settings-font-saturation-range">
       <input type="number" value="0" min="-1" max="1" step=".01" id="reader-settings-font-saturation-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _font-lightness">
       <i class="fa-solid fa-bolt-lightning reset setting-font-lightness-reset" title="<?php esc_attr_e( 'Font Lightness', 'fictioneer' ); ?>" id="reader-settings-font-lightness-reset"></i>
       <input type="range" value="0" min="-1" max="1" step=".01" class="setting-font-lightness-range" id="reader-settings-font-lightness-range">
       <input type="number" value="0" min="-1" max="1" step=".01" class="setting-font-lightness-text" id="reader-settings-font-lightness-text">
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _fonts">
       <i class="fa-solid fa-font reset" title="<?php esc_attr_e( 'Font', 'fictioneer' ); ?>" id="reader-settings-font-reset"></i>
       <div class="select-wrapper">
         <select id="reader-settings-font-select" name="fonts">
@@ -95,7 +95,7 @@ defined( 'ABSPATH' ) OR exit;
       <button type="button" id="reader-settings-next-font" value="1" class="button button--right font-stepper"><i class="fa-solid fa-caret-right"></i></button>
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _colors">
       <i class="fa-solid fa-palette reset" title="<?php esc_attr_e( 'Color', 'fictioneer' ); ?>" id="reader-settings-font-color-reset"></i>
       <div class="select-wrapper">
         <select id="reader-settings-font-color-select" name="colors">
@@ -113,7 +113,7 @@ defined( 'ABSPATH' ) OR exit;
       <button type="button" value="1" id="reader-settings-next-font-color" class="button button--right font-color-stepper"><i class="fa-solid fa-caret-right"></i></button>
     </div>
 
-    <div class="modal__row reader-settings__row _vertical-shrink-spacing">
+    <div class="modal__row reader-settings__row _vertical-shrink-spacing _font-weight">
       <i class="fa-solid fa-weight-hanging reset font-weight-reset"></i>
       <div class="select-wrapper">
         <select name="font-weight" class="site-setting-font-weight">
