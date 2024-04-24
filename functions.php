70f9716f2ad2f62b771c405d383c41516d56d91e
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-04-24 21:20:20 +0200

    Bump version to 5.14.0

--- functions.php
+++ functions.php
@@ -32,9 +32,9 @@ if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 // =============================================================================
 
 // Version
-define( 'FICTIONEER_VERSION', '5.13.0' );
+define( 'FICTIONEER_VERSION', '5.14.0' );
 define( 'FICTIONEER_MAJOR_VERSION', '5' );
-define( 'FICTIONEER_RELEASE_TAG', 'v5.13.0' );
+define( 'FICTIONEER_RELEASE_TAG', 'v5.14.0' );
 
 if ( ! defined( 'CHILD_VERSION' ) ) {
   define( 'CHILD_VERSION', null );
@@ -688,5 +688,3 @@ if ( ! is_admin() ) {
 
   require_once __DIR__ . '/includes/functions/hooks/_mobile_menu_hooks.php';
 }
-
-?>
