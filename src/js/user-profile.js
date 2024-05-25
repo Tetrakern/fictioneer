b0b71920af73eecccdf7b4eb981dcf32465e3418
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-05-25 14:48:58 +0200

    Clean up view early if user deletes account
    Otherwise, it can look like the user is still logged after reloading.

--- src/js/user-profile.js
+++ src/js/user-profile.js
@@ -108,6 +108,8 @@ function fcn_deleteMyAccount(button) {
   .then(response => {
     if (response.success) {
       // Successfully deleted
+      localStorage.removeItem('fcnAuth');
+      localStorage.removeItem('fcnProfileAvatar');
       location.reload();
     } else {
       // Could not be deleted
