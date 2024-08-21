124633565b632cce7c8fa442b0bbc6b029ae5717
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-08-22 00:44:05 +0200

    Add comment for clarification

--- includes/functions/_setup-theme.php
+++ includes/functions/_setup-theme.php
@@ -892,7 +892,7 @@ function fictioneer_style_queue() {
       // Shortcodes
       if (
         preg_match( '/\[fictioneer_latest_[^\]]*type="list"[^\]]*\]/', $post->post_content ) ||
-        is_admin()
+        is_admin() // Accounts for page editors like Elementor
       ) {
         wp_enqueue_style(
           'fictioneer-post-list',
