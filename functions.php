29ea434c91e67b9e1fe7131315d3ba13dcbbebb3
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-09-24 18:58:09 +0200

    Bumb version to 5.24.2 (hotfix)

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@
 // =============================================================================
 
 // Version
-define( 'FICTIONEER_VERSION', '5.24.1' );
+define( 'FICTIONEER_VERSION', '5.24.2' );
 define( 'FICTIONEER_MAJOR_VERSION', '5' );
-define( 'FICTIONEER_RELEASE_TAG', 'v5.24.1' );
+define( 'FICTIONEER_RELEASE_TAG', 'v5.24.2' );
 
 if ( ! defined( 'CHILD_VERSION' ) ) {
   define( 'CHILD_VERSION', null );
