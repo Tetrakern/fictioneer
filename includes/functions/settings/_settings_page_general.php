de90dda2ad544f8942a9169bdbed2b19e792f2b0
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-12-08 14:35:16 +0100

    Update _settings_page_general.php

--- includes/functions/settings/_settings_page_general.php
+++ includes/functions/settings/_settings_page_general.php
@@ -1100,7 +1100,7 @@ $images = get_template_directory_uri() . '/img/documentation/';
                     __( 'Recommended. Significantly faster frontend user setup, but can cause issues in rare cases.', 'fictioneer' ),
                     sprintf(
                       __( 'FFCNR stands for <em>Fast Fictioneer Requests</em>, an alternative entry point to WordPress with minimal environment. In this mode, almost nothing is loaded: no themes, no plugins, and only a tiny subset of WordPress itself. This reduced environment significantly improves performance for certain tasks, such as fetching user data for the frontend. Refer to the <a href="%s" target="_blank" rel="noopener noreferrer">installation guide</a> for details on interacting with FFCNR.', 'fictioneer' ),
-                      'https://github.com/Tetrakern/fictioneer/blob/main/INSTALLATION.md'
+                      'https://github.com/Tetrakern/fictioneer/blob/main/INSTALLATION.md#ffcnr-fast-fictioneer-requests'
                     )
                   );
                 ?>
