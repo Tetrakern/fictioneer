112ac8d5d6582f00479c75fdc87f4ba835591d47
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2025-02-19 19:13:07 +0100

    Fix, improve, and reduce sharing media buttons

--- partials/_modal-sharing.php
+++ partials/_modal-sharing.php
@@ -7,6 +7,7 @@
  * @since 4.0.0
  * @since 5.14.0 - Account for redirect link in story.
  * @since 5.25.0 - Added Bluesky, removed WhatsApp and Telegram, reordered buttons.
+ * @since 5.27.4 - Refactored output of links.
  */
 
 
@@ -14,7 +15,35 @@
 defined( 'ABSPATH' ) OR exit;
 
 // Setup
-$story_link = get_post_meta( get_the_ID(), 'fictioneer_story_redirect_link', true ) ?: get_permalink( get_the_ID() );
+$post_id = get_the_ID();
+$title = fictioneer_get_safe_title( $post_id );
+$story_link = get_post_meta( $post_id, 'fictioneer_story_redirect_link', true ) ?: get_permalink( $post_id );
+$output = [];
+
+// Links
+if ( ! get_option( 'fictioneer_disable_bluesky_share' ) ) {
+  $output['bluesky'] = '<a href="https://bsky.app/intent/compose?text=' . $story_link . '" target="_blank" rel="noopener nofollow" class="media-buttons__item bluesky"><i class="fab fa-bluesky"></i></a>';
+}
+
+if ( ! get_option( 'fictioneer_disable_twitter_share' ) ) {
+  $output['twitter'] = '<a href="https://twitter.com/intent/tweet/?text=' . urlencode( $title ) . '&url=' . $story_link . '" target="_blank" rel="noopener nofollow" class="media-buttons__item twitter"><i class="fab fa-twitter"></i></a>';
+}
+
+if ( ! get_option( 'fictioneer_disable_tumblr_share' ) ) {
+  $output['tumblr'] = '<a href="http://tumblr.com/widgets/share/tool?canonicalUrl=' . $story_link . '" target="_blank" rel="noopener nofollow" class="media-buttons__item tumblr"><i class="fa-brands fa-tumblr-square"></i></a>';
+}
+
+if ( ! get_option( 'fictioneer_disable_reddit_share' ) ) {
+  $output['reddit'] = '<a href="http://www.reddit.com/submit?url=' . $story_link . '&title=' . urlencode( $title ) . '" target="_blank" rel="noopener nofollow" class="media-buttons__item reddit"><i class="fa-brands fa-reddit"></i></a>';
+}
+
+if ( ! get_option( 'fictioneer_disable_mastodon_share' ) ) {
+  $output['mastodon'] = '<a href="https://toot.kytta.dev/?text=Check%20out%20' . $story_link . '" target="_blank" rel="noopener nofollow" class="media-buttons__item mastodon"><i class="fa-brands fa-mastodon"></i></a>';
+}
+
+if ( ! get_option( 'fictioneer_disable_facebook_share' ) ) {
+  $output['facebook'] = '<a href="https://www.facebook.com/sharer/sharer.php?u=' . $story_link . '" target="_blank" rel="noopener nofollow" class="media-buttons__item facebook"><i class="fab fa-facebook"></i></a>';
+}
 
 ?>
 
@@ -25,43 +54,11 @@ $story_link = get_post_meta( get_the_ID(), 'fictioneer_story_redirect_link', tru
 
     <div class="modal__header drag-anchor"><?php _ex( 'Share', 'Share modal heading.', 'fictioneer' ); ?></div>
 
-    <div class="modal__row media-buttons _modal">
-      <?php if ( ! get_option( 'fictioneer_disable_bluesky_share' ) ) : ?>
-        <a href="https://bsky.app/intent/compose?text=<?php echo $story_link; ?>" target="_blank" rel="noopener nofollow" class="media-buttons__item bluesky">
-          <i class="fab fa-bluesky"></i>
-        </a>
-      <?php endif; ?>
-
-      <?php if ( ! get_option( 'fictioneer_disable_twitter_share' ) ) : ?>
-        <a href="https://twitter.com/intent/tweet/?text=<?php the_title(); ?>&url=<?php echo $story_link; ?>" target="_blank" rel="noopener nofollow" class="media-buttons__item twitter">
-          <i class="fab fa-twitter"></i>
-        </a>
-      <?php endif; ?>
-
-      <?php if ( ! get_option( 'fictioneer_disable_tumblr_share' ) ) : ?>
-        <a href="http://tumblr.com/widgets/share/tool?canonicalUrl=<?php echo $story_link; ?>" target="_blank" rel="noopener nofollow" class="media-buttons__item tumblr">
-          <i class="fa-brands fa-tumblr-square"></i>
-        </a>
-      <?php endif; ?>
-
-      <?php if ( ! get_option( 'fictioneer_disable_reddit_share' ) ) : ?>
-        <a href="http://www.reddit.com/submit?url=<?php echo $story_link; ?>&title=<?php echo urlencode( get_the_title() ); ?>" target="_blank" rel="noopener nofollow" class="media-buttons__item reddit">
-          <i class="fa-brands fa-reddit"></i>
-        </a>
-      <?php endif; ?>
-
-      <?php if ( ! get_option( 'fictioneer_disable_mastodon_share' ) ) : ?>
-        <a href="https://toot.kytta.dev/?text=Check%20out%20<?php echo $story_link; ?>" target="_blank" rel="noopener nofollow" class="media-buttons__item mastodon">
-          <i class="fa-brands fa-mastodon"></i>
-        </a>
-      <?php endif; ?>
-
-      <?php if ( ! get_option( 'fictioneer_disable_facebook_share' ) ) : ?>
-        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $story_link; ?>" target="_blank" rel="noopener nofollow" class="media-buttons__item facebook">
-          <i class="fab fa-facebook"></i>
-        </a>
-      <?php endif; ?>
-    </div>
+    <?php
+      if ( ! empty( $output ) ) {
+        echo '<div class="modal__row media-buttons _modal">' . implode( '', $output ) . '</div>';
+      }
+    ?>
 
     <div class="modal__row">
       <input type="text" value="<?php echo $story_link; ?>" data-action="click->fictioneer#copyInput" data-message="<?php _e( 'Link copied to clipboard!', 'fictioneer' ); ?>" name="permalink" readonly>
